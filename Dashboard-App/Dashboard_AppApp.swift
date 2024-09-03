513c1d9f4368c4e46713fdcc057170feb6401c54
JMiller7334 <Jacob-Miller@Hotmail.com>
2024-09-03 18:52:19 -0500

    Initial Commit

---
+++ Dashboard-App/Dashboard_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_AppApp.swift
+//  Dashboard-App
+//
+//  Created by Jacob Miller on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
