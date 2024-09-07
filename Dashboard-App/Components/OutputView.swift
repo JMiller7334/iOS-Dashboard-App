0bb74d8b1d446f03c46849d9291f226058bfdbf4
JMiller7334 <Jacob-Miller@Hotmail.com>
2024-09-07 17:42:29 -0500

    improved outView

--- Dashboard-App/Components/OutputView.swift
+++ Dashboard-App/Components/OutputView.swift
@@ -5,26 +5,41 @@
 //  Created by Jacob Miller on 9/4/24.
 //
 
-import Foundation
 import SwiftUI
 
 struct OutputView: View {
-    var fillerText: String = "Database output will display here.."
+    var fillerText: String = "Database output will display here."
     @Binding var outputText: String?
 
     var body: some View {
-        
-        Text("Output")
-        TextEditor(text: .constant(outputText ?? fillerText))
-            .font(.system(size: 15))
-            .disabled(true)
-            .frame(maxWidth: .infinity, minHeight: 75)
-            .padding()
-            .background(Color.clear)
+        VStack(alignment: .leading) {
+            Text("Output")
+                .font(.headline)
+                .padding(.bottom, 5)
+                .padding(.horizontal)
+
+            ScrollView {
+                Text(outputText ?? fillerText)
+                    .font(.system(size: 15))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .background(Color.clear)
+            }
+            .frame(maxWidth: .infinity, minHeight: 75, maxHeight: 300) // Adjust height as needed
             .overlay(
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(Color(.systemGray), lineWidth: 1)
             )
             .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .padding()
+    }
+}
+
+struct OutputView_Previews: PreviewProvider {
+    @State static var sampleValue: String? = nil
+    
+    static var previews: some View {
+        OutputView(outputText: $sampleValue)
     }
 }
